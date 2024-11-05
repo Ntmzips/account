b1339236cd1a65b2c3a3c7f0cc36a5b43ea3ad14
Ntmzips <carlosneon_rocha@hotmail.com>
2024-11-05 17:03:09 -0400

    equals

---
+++ scripts/painel.js
@@ -0,0 +1,101 @@
+<<<<<<< HEAD
+
+var uni = document.getElementById("unidades")
+
+
+var saida = 
+`
+<table class="table table-striped">
+        <tr>
+            <th>POSTO DE SAUDE</th>
+            <th>AGENDAMENTO SEMANAL</th>
+            <th>AGENDAMENTO MENSAL</th>
+            <th>ALERTAS</th>
+        </tr>
+
+
+        <tr>
+            <td>UPA CENTRO</td>
+            <td>14</td>
+            <td>14</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+
+        <tr>
+            <td>UPA BELA VISTA</td>
+            <td>8</td>
+            <td>10</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+
+        <tr>
+            <td>UPA SANTA MONICA</td>
+            <td>12</td>
+            <td>16</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+
+        <tr>
+            <td>UPA FISCHER</td>
+            <td>14</td>
+            <td>14</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+`
+
+
+uni.addEventListener("click", function(){
+    let show = document.getElementById("areaShow").innerHTML = saida
+})
+
+=======
+
+var uni = document.getElementById("unidades")
+
+
+var saida = 
+`
+<table class="table table-striped">
+        <tr>
+            <th>POSTO DE SAUDE</th>
+            <th>AGENDAMENTO SEMANAL</th>
+            <th>AGENDAMENTO MENSAL</th>
+            <th>ALERTAS</th>
+        </tr>
+
+
+        <tr>
+            <td>UPA CENTRO</td>
+            <td>14</td>
+            <td>14</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+
+        <tr>
+            <td>UPA BELA VISTA</td>
+            <td>8</td>
+            <td>10</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+
+        <tr>
+            <td>UPA SANTA MONICA</td>
+            <td>12</td>
+            <td>16</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+
+        <tr>
+            <td>UPA FISCHER</td>
+            <td>14</td>
+            <td>14</td>
+            <td>SISTEMA SEM DADOS DE AGENDAMENTO PARA PROXIMA SEMANA</td>
+        </tr>
+`
+
+
+uni.addEventListener("click", function(){
+    let show = document.getElementById("areaShow").innerHTML = saida
+})
+
+>>>>>>> 94b58a37e73b5d0bf9237f404a965f1592da1de4
